0761125c8c8a888290d79aedaca6101f86c1fa5b
Victor Mateevitsi <vmateevitsi@anl.gov>
2025-02-24 14:45:13 -0600

    Added fix for ALCF

--- backend/open_webui/routers/openai.py
+++ backend/open_webui/routers/openai.py
@@ -665,8 +665,10 @@ async def generate_chat_completion(
     if "max_tokens" in payload and "max_completion_tokens" in payload:
         del payload["max_tokens"]
 
+    payload.pop("stream", None)
     # Convert the modified body back to JSON
     payload = json.dumps(payload)
+    
 
     r = None
     session = None
@@ -718,12 +720,14 @@ async def generate_chat_completion(
                 ),
             )
         else:
+            print(payload)
             try:
                 response = await r.json()
             except Exception as e:
                 log.error(e)
                 response = await r.text()
-
+                response = json.loads(response)
+            print(response)
             r.raise_for_status()
             return response
     except Exception as e:
