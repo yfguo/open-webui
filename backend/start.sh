550bea33e37210dd886cd1b9b2e4208a3fb93650
Yanfei Guo <yguo@anl.gov>
2025-10-10 12:51:22 -0500

    fix start.sh

--- backend/start.sh
+++ backend/start.sh
@@ -81,14 +81,22 @@ fi
 
 if [ -f $SSL_CERTFILE ]; then
     if [ -f $SSL_KEYFILE ]; then
-        SSL_ARGS='--ssl-keyfile "$SSL_KEYFILE" --ssl-certfile "$SSL_CERTFILE"'
+        LAUNCH_SSL=yes
     fi
 fi
 
 # Run uvicorn
-WEBUI_SECRET_KEY="$WEBUI_SECRET_KEY" exec "$PYTHON_CMD" -m uvicorn open_webui.main:app \
-    --host "$HOST" \
-    --port "$PORT" \
-    $SSL_ARGS \
-    --forwarded-allow-ips '*' \
-    "${ARGS[@]}"
+if [ "x$LAUNCH_SSL" == "xyes" ]; then
+    WEBUI_SECRET_KEY="$WEBUI_SECRET_KEY" exec "$PYTHON_CMD" -m uvicorn open_webui.main:app \
+        --host "$HOST" \
+        --port "$PORT" \
+        --ssl-keyfile "$SSL_KEYFILE" --ssl-certfile "$SSL_CERTFILE" \
+        --forwarded-allow-ips '*' \
+        "${ARGS[@]}"
+else
+    WEBUI_SECRET_KEY="$WEBUI_SECRET_KEY" exec "$PYTHON_CMD" -m uvicorn open_webui.main:app \
+        --host "$HOST" \
+        --port "$PORT" \
+        --forwarded-allow-ips '*' \
+        "${ARGS[@]}"
+fi
