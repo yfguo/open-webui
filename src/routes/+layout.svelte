4b25dd9e57f7d63f088e2a1b090a4329afc3c48c
Yanfei Guo <yguo@anl.gov>
2025-10-31 16:23:44 -0500

    restore to previous version

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -754,12 +754,3 @@
 	position="top-right"
 	closeButton
 />
-
-<div class="global-banner">
-  <div class="banner-left">
-    <img src="/static/alcf-logo-h.png" alt="Argonne ALCF Logo" class="banner-logo" />
-  </div>
-  <div class="banner-right">
-    Powered by <a href="https://github.com/open-webui/open-webui" target="_blank" rel="noopener noreferrer">Open WebUI</a> and <a href="https://www.globus.org/" target="_blank" rel="noopener noreferrer">Globus</a>
-  </div>
-</div>
